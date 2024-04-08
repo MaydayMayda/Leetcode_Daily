60c7ab2275e1b927875c94d680272c398595a620
zhou <3070493170@qq.com>
2024-04-08 17:18:57 +0800

    统计无向图中无法互相到达点对数

---
+++ 2316.cpp
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @number         : 2316
+  * @title          : 统计无向图中无法互相到达点对数
+  * @attention      : 深度优先搜索、广度优先搜索、并查集、图
+  * @author         : zzzhou
+  * @date           : 2023/10/21
+  ******************************************************************************
+  */
+
+
+#include "vector"
+#include  "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        long long ans;
+
+        return ans;
+    }
+};
+
+int main() {
+    int n=3;
+    vector<vector<int>>edgs{{0,1},{0,2},{1,2}};
+    Solution s;
+    s.countPairs(n,edgs);
+    return 0;
+}
